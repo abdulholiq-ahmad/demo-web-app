7037e55887a2b7e386af7bb6734bb28d57419684
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-17 17:23:34 +0500

    add: username

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import "./App.css";
-const tg = window.Telegram.WebApp;
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/header/Header";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  const close = () => {
-    tg.close();
-  };
+  const { onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
-    <>
-      <div>
-        <button onClick={close}>Berkitish</button>
-      </div>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={"form"} element={<Form />} />
+      </Routes>
+    </div>
   );
 }
 
