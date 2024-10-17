0264a120f55a9391842fd747834b4d15e7cd9830
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-17 17:27:24 +0500

    add: header

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/header/Header";
 import { Route, Routes } from "react-router-dom";
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -16,10 +14,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route index element={<ProductList />} />
-        <Route path={"form"} element={<Form />} />
-      </Routes>
     </div>
   );
 }
